2fd89ea2e1ebeba353ab6a4ffee55d12e780febd
farzad <farza@DESKTOP-UDVL642>
2025-10-04 17:15:17 -0400

    Implemented edit and delete for categories and pies

--- BethanysPieShopAdmin/Controllers/PieController.cs
+++ BethanysPieShopAdmin/Controllers/PieController.cs
@@ -60,5 +60,84 @@ namespace BethanysPieShopAdmin.Controllers
             }
         }
 
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var allCategories = await _categoryRepository.GetAllCategoriesAsync();
+
+            IEnumerable<SelectListItem> selectListItems = new SelectList(allCategories, "CategoryId", "Name", null);
+
+            var selectedPie = await _pieRepository.GetPieByIdAsync(id.Value);
+
+            PieEditViewModel pieEditViewModel = new() { Categories = selectListItems, Pie = selectedPie };
+            return View(pieEditViewModel);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(PieEditViewModel pieEditViewModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await _pieRepository.UpdatePieAsync(pieEditViewModel.Pie);
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", $"Updating the category failed, please try again! Error: {ex.Message}");
+            }
+
+            var allCategories = await _categoryRepository.GetAllCategoriesAsync();
+
+            IEnumerable<SelectListItem> selectListItems = new SelectList(allCategories, "CategoryId", "Name", null);
+
+            pieEditViewModel.Categories = selectListItems;
+
+            return View(pieEditViewModel);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var selectedCategory = await _pieRepository.GetPieByIdAsync(id);
+
+            return View(selectedCategory);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int? pieId)
+        {
+            if (pieId == null)
+            {
+                ViewData["ErrorMessage"] = "Deleting the pie failed, invalid ID!";
+                return View();
+            }
+
+            try
+            {
+                await _pieRepository.DeletePieAsync(pieId.Value);
+                TempData["PieDeleted"] = "Pie deleted successfully!";
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewData["ErrorMessage"] = $"Deleting the pie failed, please try again! Error: {ex.Message}";
+            }
+
+            var selectedPie = await _pieRepository.GetPieByIdAsync(pieId.Value);
+            return View(selectedPie);
+        }
+
     }
 }
---
+++ BethanysPieShopAdmin/Models/ViewModels/PieEditViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BethanysPieShopAdmin.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BethanysPieShopAdmin.Models.ViewModels
+{
+    public class PieEditViewModel
+    {
+        public IEnumerable<SelectListItem>? Categories { get; set; } = default!;
+        public Pie Pie { get; set; } 
+    }
+}
